655c3602be733f7be8c56416926096adb6129887
Ofe97 <karlamariscal0920@gmail.com>
2024-06-23 17:35:26 -0600

    Sistema_Vacantes

--- src/main/java/Datos/Dao/EmpresasDAO.java
+++ src/main/java/Datos/Dao/EmpresasDAO.java
@@ -27,14 +27,37 @@ public class EmpresasDAO {
     // Editar Nombre empresa
 
     public  void  editarNombre_Empresa(int id_Empresa, String Nombre_Empresa)throws  SQLException{
-
+     String sql= "Update Empresas set Nombre_Empresa = ? where id_Empresa= ?";
+     try(PreparedStatement statement = connection.prepareStatement(sql)){
+         statement.setString(1,Nombre_Empresa);
+         statement.setInt(2,id_Empresa);
+         statement.executeUpdate();
+      }
     }
 
 
+    // Editar direccion  Empresa
+
+    public void editarDireccion(int id_Empresa, String Direccion)throws  SQLException{
+        String sql = "Update Empresas set Direccion = ? where id_Empresa = ?";
+        try(PreparedStatement statement= connection.prepareStatement(sql)){
+            statement.setString(1,Direccion);
+            statement.setInt(2,id_Empresa);
+            statement.executeUpdate();
+        }
+    }
 
 
+  //Editar Pais donde se ubica la empresa
 
-
+    public void editarPais(int id_Empresa, String Pais)throws SQLException{
+        String sql= "Update Empresas set Pais = ? where id_Empresa = ?";
+        try(PreparedStatement statement = connection.prepareStatement(sql)){
+            statement.setString(1,Pais);
+            statement.setInt(2,id_Empresa);
+            statement.executeUpdate();
+        }
+    }
 
 
 
